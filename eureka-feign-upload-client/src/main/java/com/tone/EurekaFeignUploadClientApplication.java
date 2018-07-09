e9358957fa4581902b33e22a003f5190a7a1e5b5
逛逛0277 <381347973@qq.com>
2018-07-09 18:07:06 +0800

    上传文件

---
+++ eureka-feign-upload-client/src/main/java/com/tone/EurekaFeignUploadClientApplication.java
@@ -0,0 +1,15 @@
+package com.tone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaFeignUploadClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaFeignUploadClientApplication.class, args);
+    }
+}
